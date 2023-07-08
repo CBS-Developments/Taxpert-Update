c0d8467f5ba15502c0ccd7c37098768493722b6b
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-07-08 20:09:00 +0530

    Create login_button.dart

---
+++ taxpert_update/lib/Components/login_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../Sizes/cal_page_sizes.dart';
+
+class LoginButton extends StatelessWidget {
+
+  final Function()? onTap;
+  final String childText;
+  const LoginButton({Key? key, this.onTap, required this.childText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: buttonHeight(context),
+        width: buttonWidth(context),
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.white,
+            width: 1.0,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            childText,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: buttonFont(context),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

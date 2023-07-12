f5dfa65087652a03d828234f64a7aa1ffa50cbda
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-07-13 00:12:53 +0530

    Create new_readio_container.dart

---
+++ taxpert_update/lib/Components/new_readio_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../Sizes/login_page_sizes.dart';
+
+class NewRedioContainer extends StatelessWidget {
+  const NewRedioContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(textFMargin(context)),
+      color: Colors.white70,
+      width: textFWidth(context),
+      height: textFHeight(context),
+
+
+    );
+  }
+}

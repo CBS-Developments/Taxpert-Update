c6718e42037938fc11b0e8eba5e862530d2bbd8f
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-07-13 23:00:15 +0530

    Update my_Appbar.dart

--- taxpert_update/lib/Components/my_Appbar.dart
+++ taxpert_update/lib/Components/my_Appbar.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:taxpert_update/Sizes/create_account_page_sizes.dart';
 
 import '../Sizes/dashboard_sizes.dart';
-
-
 
 PreferredSizeWidget myAppBar(BuildContext context) {
   return PreferredSize(
     preferredSize: Size.fromHeight(appBarHeight(context)),
     child: AppBar(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
+      automaticallyImplyLeading: false,
       title: Text(
-        'CBS Task System',
+        'Taxperts',
         style: TextStyle(
           fontSize: appBarFont(context),
-          color: Colors.black,
+          color: Colors.white,
           fontWeight: FontWeight.bold,
         ),
       ),
+      actions: [
+        Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Profile Name',
+            style: TextStyle(
+              fontSize: appBarFont(context),
+              color: Colors.white,
+              // fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: smallBoxWidth(context),
+        ),
+        IconButton(
+          iconSize: appBarIconSize(context),
+          onPressed: () {},
+          icon: Icon(
+            Icons.person_3_outlined,
+            color: Colors.white,
+          ),
+          tooltip: 'View Profile',
+        ),
+        SizedBox(
+          width: smallBoxWidth(context),
+        ),
+      ],
     ),
   );
 }

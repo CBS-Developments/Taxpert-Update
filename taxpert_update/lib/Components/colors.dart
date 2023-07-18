b7c9438ee6c186917838cd39ebf42ac4a4bb2af7
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-07-18 23:03:36 +0530

    Update colors.dart

--- taxpert_update/lib/Components/colors.dart
+++ taxpert_update/lib/Components/colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColor{
   static  Color normalGreen=const Color(0xFF1A932E);
   static  Color highLightGreen=const Color(0xFF00EA25);
+  static  Color LightGreen=const Color(0xFF00EA10);
 }
